8e399700ab9dedc0c4be8753f79cdc477539d481
Angelo Andrey Vásquez Cortés <avasquez@coem.co>
2017-10-11 09:24:15 -0500

    Agregar archivos de proyecto.

---
+++ SoapServiceAES/IService1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SoapServiceAES
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        decimal Calculator(CalculatorClass calculator);
+
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+   
+    [DataContract]
+
+    public class CalculatorClass
+    {
+        decimal _FirstNumber;
+        decimal _SecondNumber;
+        
+        [DataMember]
+        public decimal FirstNumber { get => _FirstNumber; set => _FirstNumber = value; }
+        [DataMember]
+        public decimal SecondNumber { get => _SecondNumber; set => _SecondNumber = value; }
+    }
+}
---
+++ SoapServiceAES/Service1.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SoapServiceAES
+{
+    public class Service1 : IService1
+    {
+        public decimal Calculator(CalculatorClass calculator)
+        {
+
+            decimal response = 0m;
+            response = calculator.FirstNumber + calculator.SecondNumber;
+            return response;
+        }
+    }
+}
