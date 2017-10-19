87f37c626d56b2d0921972488038914b413461fe
avasquez <angelovasquez@outlook.com>
2017-10-19 17:08:56 -0500

    ajuste archivos sh

--- SoapServiceAES/IService1.cs
+++ SoapServiceAES/IService1.cs
@@ -8,7 +8,6 @@ using System.Text;
 
 namespace SoapServiceAES
 {
-    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
     [ServiceContract]
     public interface IService1
     {
